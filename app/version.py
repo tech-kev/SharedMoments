03d2c10dfda68e15604d086f22d628dfed17db6c
tech-kev <github@kevin-theil.de>
2026-03-24 20:46:23 +0100

    update version and edition to capitalized

--- app/version.py
+++ app/version.py
@@ -1,9 +1,9 @@
 """Single source of truth for the SharedMoments version."""
 
-__version__ = '2.0.0-alpha'
+__version__ = '2.0.0'
 
 # Für Semver-Vergleiche
 VERSION_MAJOR = 2
 VERSION_MINOR = 0
 VERSION_PATCH = 0
-VERSION_SUFFIX = 'alpha'  # 'alpha', 'beta', 'rc1', '' (leer = stable release)
+VERSION_SUFFIX = ''  # 'alpha', 'beta', 'rc1', '' (leer = stable release)
